0283284a1ddfb12388fce605e14a63a9674c160c
zwoosks <zwoosks@protonmail.com>
2023-02-23 20:30:50 +0100

    Initial commit

---
+++ src/main/java/ne/zwoosks/blockbattle/api/BlockBattle.java
@@ -0,0 +1,9 @@
+package ne.zwoosks.blockbattle.api;
+
+import org.bukkit.plugin.Plugin;
+
+public interface BlockBattle extends Plugin {
+
+    // todo all the getters needed
+
+}
---
+++ src/main/java/ne/zwoosks/blockbattle/api/events/ArenaPlayerDeathEvent.java
@@ -0,0 +1,4 @@
+package ne.zwoosks.blockbattle.api.events;
+
+public class ArenaPlayerDeathEvent {
+}
